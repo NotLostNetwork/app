6475430de42c755bcd239a29fd018175167e03df
Shestoy06 <pochta5550@gmail.com>
2024-12-21 23:02:08 +0100

    try fix viewport shrink on keyboard appear

--- web/app/pages/graph/index.tsx
+++ web/app/pages/graph/index.tsx
@@ -1,21 +1,48 @@
+import { Input } from "@telegram-apps/telegram-ui"
 import TWallpaper, { TWallpaperHandlers } from "@twallpaper/react"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function GraphPage() {
-  const ref = useRef<TWallpaperHandlers>(null)
+
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newHeight = window.visualViewport
+        ? window.visualViewport.height
+        : window.innerHeight;
+      setViewportHeight(newHeight);
+    };
+
+    // Add event listeners for viewport resizing
+    window.visualViewport?.addEventListener('resize', handleResize);
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup listeners on unmount
+    return () => {
+      window.visualViewport?.removeEventListener('resize', handleResize);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
-    <TWallpaper
-      ref={ref}
-      options={{
-        fps: 120,
-        tails: 30,
-        colors: ["#efd359", "#e984d8", "#ac86ed", "#40cdde"],
-        pattern: {
-          mask: true,
-          size: "300px",
-          image: "https://twallpaper.js.org/patterns/tattoos.svg",
-        },
-      }}
+    <div style={{
+      height: viewportHeight,
+      overflow: 'hidden',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+    }}>
+      Test viewport
+      <Input
+        autoFocus={true}
+        className=" p-0 text-white bg-gray-800"
+        style={{ color: "white" }}
+        type="text"
+        placeholder="Tags"
+        value={"tagsValue"}
+        onChange={(e) => {}}
     />
+    </div>
   )
 }
